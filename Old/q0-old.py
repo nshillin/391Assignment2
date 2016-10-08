83c739eabc27eb38c4f4b188028be0bd0244b2f3
Noah Shillington <nshillin@ualberta.ca>
2016-10-08 13:25:08 -0600

    Q0.py translates to 1000x1000 grid

--- Old/q0-old.py
+++ Old/q0-old.py
@@ -12,15 +12,22 @@ k_earthRadius = float(6371008)
 change = 10/2
 writeFile = open('poi_edited.tsv', 'w')
 fileName = "poi.tsv"
+bigLat = float(0)
+bigLon = float(0)
 with open(fileName) as f:
     for line in f:
         lineList = line.split()
         lat = float(lineList[2])
         lon = float(lineList[3])
+        if lat>bigLat:
+            bigLat = lat
+        if lon>bigLon:
+            bigLon = lon
         outputList = [lineList[0],lineList[1]]
         outputList.append(newLatitude(lat,-change))
         outputList.append(newLatitude(lat,change))
         outputList.append(newLongitude(lon,lat,-change))
         outputList.append(newLongitude(lon,lat,change))
         writeFile.write('\t'.join(outputList)+'\n')
+print bigLat, bigLon
 writeFile.close()
