871422be28574cc4a2fbd1e9fa658457c4c41aee
jero1994 <jero1994@users.noreply.github.com>
2016-10-30 01:05:17 -0600

    further q7

--- q7.c
+++ q7.c
@@ -5,7 +5,6 @@
 
 float mindist(float point[2], float rect[2][2]); //rect is 2 coordinates, each 2 values
 float minmaxdist(float point[2], float rect[2][2]);
-void nnRecursive(int nodeno, sqlite3 *db, sqlite3_stmt *stmt);
 
 typedef struct {
 	int nodeno;
@@ -16,6 +15,8 @@ typedef struct {
 	float MinDist;
 	float MinMaxDist;
 } Branch;
+
+Branch nnRecursive(int nodeno, sqlite3 *db, sqlite3_stmt *stmt);
 
 int main(int argc, char **argv) {
 
@@ -90,29 +91,100 @@ float minmaxdist(float point[2], float rect[2][2]) {
 	return result;
 }
 
-void nnRecursive(int nodeno, sqlite3 *db, sqlite3_stmt *stmt) {
+Branch nnRecursive(int nodeno, sqlite3 *db, sqlite3_stmt *stmt) {
 	float nnDist = INFINITY;
-	char *getChildren;
-	sprintf (getChildren, "SELECT nodeno \
-		FROM poirtree_parent \
-		WHERE parentnode = %i;", nodeno);
 	
-	Branch *abl = malloc(sizeof(*abl) * 100);
+	char leafCheck[255];
+	char getChildren[1000];
+	char getObjects[255];
+	sprintf(leafCheck, "SELECT COUNT(*) FROM poirtree_parent \
+			WHERE parentnode = %i", nodeno); //Number of child nodes (leaf if 0)
+	sprintf(getChildren, "SELECT pp.nodeno, MIN(minX), MAX(maxX), MIN(minY), MAX(maxY) \
+			FROM poirtree p, poirtree_rowid pr, poirtree_parent pp \
+			WHERE id = rowid \
+			AND pp.parentnode = %i \
+			AND (pr.nodeno = pp.nodeno \
+			OR pr.nodeno IN \
+			(WITH RECURSIVE rChild(nodeno, parentnode) AS ( \
+			SELECT nodeno, parentnode FROM poirtree_parent UNION \
+			SELECT rChild.nodeno, poirtree_parent.parentnode \
+			FROM poirtree_parent, rChild \
+			WHERE rChild.parentnode = poirtree_parent.nodeno) \
+			SELECT nodeno FROM rChild \
+			WHERE parentnode = pp.nodeno)) \
+			GROUP BY pp.nodeno;", nodeno); //MBR of all objects of each child node (direct or indirect)
+	sprintf(getObjects, "SELECT id, minX, maxX, minY, maxY \
+			FROM poirtree, poirtree_rowid \
+			WHERE id = rowid AND nodeno = %i", nodeno); //MBRs of each object of this node
 	
-	check = sqlite3_prepare_v2(db, getChildren, -1, &stmt, 0);
+	Branch *nodes = malloc(sizeof(*abl) * 100); //Child nodes
+	Branch *objects = malloc(sizeof(*abl) * 100); //Objects (if node is a leaf, or for returns of recursive nn searches)
+	
+	check = sqlite3_prepare_v2(db, leafCheck, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
 		return(1);
 	}
+	bool isLeaf = ((check = sqlite3_step(stmt)) != SQLITE_ROW); //true if node is a leaf
+	sqlite3_finalize(stmt);
 	
-	int abl_i = 0;
-	while((check = sqlite3_step(stmt)) == SQLITE_ROW) {
-		Branch b;
-		b.nodeno = sqlite3_column_int(stmt, 0);
-		abl[abl_i] = b;
-		abl_i++;
+	float min_minmaxDist = INFINITY; //Smallest minmaxdist, against which mindist values are measured in pruning 1.
+	
+	int nodes_i = 0;
+	int objects_i = 0;
+	
+	if (isLeaf) {
+		check = sqlite3_prepare_v2(db, getChildren, -1, &stmt, 0);
+		if(check != SQLITE_OK) {
+			fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
+			sqlite3_close(db);
+			return(1);
+		}
+		while((check = sqlite3_step(stmt)) == SQLITE_ROW) {
+			Branch b;
+			b.nodeno = sqlite3_column_int(stmt, 0);
+			b.minX = (float)sqlite3_column_double(stmt, 1);
+			b.maxX = (float)sqlite3_column_double(stmt, 2);
+			b.minY = (float)sqlite3_column_double(stmt, 3);
+			b.maxY = (float)sqlite3_column_double(stmt, 4);
+			
+			float rect[2][2] = {{b.minX,b.maxX},{b.minY,b.maxY}};
+			
+			b.MinDist = mindist(float point[2], float rect[2][2]);
+			b.MinMaxDist = minmaxdist(float point[2], float rect[2][2]);
+			if (b.MinMaxDist < min_minmaxDist) { min_minmaxDist = b.MinMaxDist; }
+			
+			nodes[nodes_i] = b;
+			nodes_i++;
+		}
+	} else {
+		check = sqlite3_prepare_v2(db, getObjects, -1, &stmt, 0);
+		if(check != SQLITE_OK) {
+			fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
+			sqlite3_close(db);
+			return(1);
+		}
+		while((check = sqlite3_step(stmt)) == SQLITE_ROW) {
+			Branch b;
+			b.nodeno = sqlite3_column_int(stmt, 0);
+			b.minX = (float)sqlite3_column_double(stmt, 1);
+			b.maxX = (float)sqlite3_column_double(stmt, 2);
+			b.minY = (float)sqlite3_column_double(stmt, 3);
+			b.maxY = (float)sqlite3_column_double(stmt, 4);
+			
+			float rect[2][2] = {{b.minX,b.maxX},{b.minY,b.maxY}};
+			
+			b.MinDist = mindist(float point[2], float rect[2][2]);
+			b.MinMaxDist = minmaxdist(float point[2], float rect[2][2]);
+			if (b.MinMaxDist < min_minmaxDist) { min_minmaxDist = b.MinMaxDist; }
+			
+			objects[objects_i] = b;
+			objects_i++;
+		}
 	}
-	
-	free (abl);
+
+	free (nodes);
+	free (objects);
+	return NULL;
 }
