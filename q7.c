94a42979b2ccc5123ec0974ee74ea7a446b2fa35
jero1994 <jero1994@users.noreply.github.com>
2016-10-30 23:14:33 -0600

    regex stuff

--- q7.c
+++ q7.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 #include "sqlite3.h"
 
 float mindist(float point[2], float rect[2][2]); //rect is 2 coordinates, each 2 values
@@ -9,10 +10,6 @@ float minmaxdist(float point[2], float rect[2][2]);
 
 typedef struct node {
 	long nodeno;
-	float minX;
-	float maxX;
-	float minY;
-	float maxY;
 	float MinDist;
 	float MinMaxDist;
 	struct node *prev;
@@ -39,26 +36,8 @@ int main(int argc, char **argv) {
 		sqlite3_close(db);
 		return(1);
 	}
-	char *root_select = "SELECT nodeno FROM poirtree_node WHERE nodeno NOT IN \
-			(SELECT nodeno FROM poirtree_parent);"; //Select the root node, which will be absent from poirtree_parent
 	
-	check = sqlite3_prepare_v2(db, root_select, -1, &stmt, 0);
-	if(check != SQLITE_OK) {
-		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return(1);
-	}
-	int root_no;
-	while((check = sqlite3_step(stmt)) == SQLITE_ROW) { root_no = sqlite3_column_int(stmt, 0); }
-	sqlite3_finalize(stmt);
-
-	Branch *nn = nnRecursive(root_no, coord, db, stmt);
-	
-	if (nn != NULL) {
-		fprintf(stdout, "%li|%f|%f|%f|%f",nn->nodeno,nn->minX,nn->maxX,nn->minY,nn->maxY);
-	} else {
-		printf("nn is null, fix\n");
-	}
+	Branch *nn = nnRecursive(1, coord, db, stmt);
 	
 	sqlite3_close(db);
 	
@@ -101,212 +80,39 @@ float minmaxdist(float point[2], float rect[2][2]) {
 Branch *nnRecursive(long nodeno, float point[2], sqlite3 *db, sqlite3_stmt *stmt) {
 	fprintf(stdout, "Beginning nn on node %li\n", nodeno);
 	int check;
+	regex_t reg;
+	size_t numOfMatches = 0;
+	regmatch_t matches[100];
 	float nnDist = INFINITY;
 	Branch *nn = NULL;
 	
-	char leafCheck[255];
-	char getChildren[1000];
-	char getObjects[255];
-	sprintf(leafCheck, "SELECT * FROM poirtree_parent \
-			WHERE parentnode = %li;", nodeno); //Number of child nodes (leaf if 0)
-	sprintf(getChildren, "SELECT pp.nodeno, MIN(minX), MAX(maxX), MIN(minY), MAX(maxY) \
-			FROM poirtree p, poirtree_rowid pr, poirtree_parent pp \
-			WHERE id = rowid \
-			AND pp.parentnode = %li \
-			AND (pr.nodeno = pp.nodeno \
-			OR pr.nodeno IN \
-			(WITH RECURSIVE rChild(nodeno, parentnode) AS ( \
-			SELECT nodeno, parentnode FROM poirtree_parent UNION \
-			SELECT rChild.nodeno, poirtree_parent.parentnode \
-			FROM poirtree_parent, rChild \
-			WHERE rChild.parentnode = poirtree_parent.nodeno) \
-			SELECT nodeno FROM rChild \
-			WHERE parentnode = pp.nodeno)) \
-			GROUP BY pp.nodeno;", nodeno); //MBR of all objects of each child node (direct or indirect)
-	sprintf(getObjects, "SELECT id, minX, maxX, minY, maxY \
-			FROM poirtree, poirtree_rowid \
-			WHERE id = rowid AND nodeno = %li;", nodeno); //MBRs of each object of this node
+	char getChildren[255];
+	sprintf(getChildren, "SELECT rtreenode(2,data) FROM poirtree_node \
+			WHERE nodeno = %li;", nodeno); //MBRs of all children of nodeno
 	
-	Branch *nodes_head = NULL; //Linked list of nodes - head
-	Branch *nodes_tail = NULL; //Linked list of nodes - tail
-	Branch *objects_head = NULL; //Linked list of objects - head
-	Branch *objects_tail = NULL; //Linked list of objects - tail
+	float min_minmaxDist = INFINITY; //Smallest minmaxdist, against which mindist values are measured in pruning 1.
 	
-	check = sqlite3_prepare_v2(db, leafCheck, -1, &stmt, 0);
+	check = sqlite3_prepare_v2(db, getChildren, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
 		return(NULL);
 	}
-	int isLeaf = ((check = sqlite3_step(stmt)) != SQLITE_ROW); //true if node is a leaf
+	if((check = sqlite3_step(stmt)) == SQLITE_ROW) { //true if node, false if object
+		regcomp(&reg, "{[[:digit][:space][:punct]]+}", 0); //placeholder, not fully useful yet but should match in theory
+		
+		while ((check = regexec(&reg, sqlite3_column_text(stmt, 0), numOfMatches, matches, 0)) == 0) {
+			printf("%i\n", (int)numOfMatches);
+			for (int i = 0; i < numOfMatches; i++) {
+				printf("%i\n", i);
+			}
+		}
+	} else {
+		return(NULL);
+	}
 	sqlite3_finalize(stmt);
 	
-	float min_minmaxDist = INFINITY; //Smallest minmaxdist, against which mindist values are measured in pruning 1.
-	
-	if (!isLeaf) {
-		printf("Node\n");
-		check = sqlite3_prepare_v2(db, getChildren, -1, &stmt, 0);
-		if(check != SQLITE_OK) {
-			fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
-			sqlite3_close(db);
-			return(NULL);
-		}
-		while((check = sqlite3_step(stmt)) == SQLITE_ROW) {
-			Branch *b = (Branch*)malloc(sizeof(Branch));
-
-			b->nodeno = (long)sqlite3_column_int64(stmt, 0);
-			b->minX = (float)sqlite3_column_double(stmt, 1);
-			b->maxX = (float)sqlite3_column_double(stmt, 2);
-			b->minY = (float)sqlite3_column_double(stmt, 3);
-			b->maxY = (float)sqlite3_column_double(stmt, 4);
-			b->next = NULL;
-			float rect[2][2] = {{b->minX,b->maxX},{b->minY,b->maxY}};
-			
-			b->MinDist = mindist(point, rect);
-			b->MinMaxDist = minmaxdist(point, rect);
-			if (b->MinMaxDist < min_minmaxDist) { min_minmaxDist = b->MinMaxDist; }
-			if (nodes_head == NULL) { nodes_head = b; }
-			if (nodes_tail != NULL) { nodes_tail->next = b; }
-			b->prev = nodes_tail;
-			nodes_tail = b;
-			printf("%li ", b->nodeno);
-		}
-		printf("\n");
-		sqlite3_finalize(stmt);
-	} else {
-		printf("Leaf node\n");
-		check = sqlite3_prepare_v2(db, getObjects, -1, &stmt, 0);
-		if(check != SQLITE_OK) {
-			fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
-			sqlite3_close(db);
-			return(NULL);
-		}
-		while((check = sqlite3_step(stmt)) == SQLITE_ROW) {
-			Branch *b = (Branch*)malloc(sizeof(Branch));
-			
-			b->nodeno = (long)sqlite3_column_int64(stmt, 0);
-			b->minX = (float)sqlite3_column_double(stmt, 1);
-			b->maxX = (float)sqlite3_column_double(stmt, 2);
-			b->minY = (float)sqlite3_column_double(stmt, 3);
-			b->maxY = (float)sqlite3_column_double(stmt, 4);
-			b->next = NULL;
-			float rect[2][2] = {{b->minX,b->maxX},{b->minY,b->maxY}};
-			
-			b->MinDist = mindist(point, rect);
-			b->MinMaxDist = minmaxdist(point, rect);
-			if (b->MinMaxDist < min_minmaxDist) { min_minmaxDist = b->MinMaxDist; }
-			
-			if (objects_head == NULL) { objects_head = b; }
-			if (objects_tail != NULL) { objects_tail->next = b; }
-			b->prev = objects_tail;
-			objects_tail = b;
-			printf("%li ", b->nodeno);
-		}
-		printf("\n");
-		sqlite3_finalize(stmt);
-	}
-	//Pruning 1: Remove MBRs farther than furthest point of nearest MBR
-	Branch *ll_pointer = nodes_head;
-	float nodes_minminmax = pruning_min_minmaxdist(nodes_head);
-	while (ll_pointer != NULL) {
-		if (ll_pointer->MinDist > nodes_minminmax) {
-			//Remove link
-			Branch *buffer = ll_pointer;
-			ll_pointer = ll_pointer->next;
-			if (buffer->prev == NULL) { nodes_head = buffer->next; }
-			else { buffer->prev->next = buffer->next; }
-			if (buffer->next == NULL) { nodes_tail = buffer->prev; }
-			else { buffer->next->prev = buffer->prev; }
-			free(buffer);
-		} else { ll_pointer = ll_pointer->next; }
-	}
-	//Pruning 2: Remove objects farther than furthest point of nearest MBR
-	ll_pointer = objects_head;
-	while (ll_pointer != NULL) {
-		if (ll_pointer->MinDist > nodes_minminmax) {
-			//Remove link
-			Branch *buffer = ll_pointer;
-			ll_pointer = ll_pointer->next;
-			if (buffer->prev == NULL) { objects_head = buffer->next; }
-			else { buffer->prev->next = buffer->next; }
-			if (buffer->next == NULL) { objects_tail = buffer->prev; }
-			else { buffer->next->prev = buffer->prev; }
-			free(buffer);
-		} else { ll_pointer = ll_pointer->next; }
-	}
-	ll_pointer = nodes_head;
-	while (ll_pointer != NULL) {
-		//Run nearestneighbor on branch, add to objects (will return object), delete branch
-		Branch *b = nnRecursive(ll_pointer->nodeno, point, db, stmt);
-		if (b == NULL) { printf("Why is b null?\n"); }
-		else { printf("b is %li\n", b->nodeno); }
-		if (objects_head == NULL) { objects_head = b; }
-		if (objects_tail != NULL) { objects_tail->next = b; }
-		b->prev = objects_tail;
-		objects_tail = b;
-
-		Branch *buffer = ll_pointer;
-		ll_pointer = ll_pointer->next;
-		if (buffer->prev == NULL) { nodes_head = buffer->next; }
-		else { buffer->prev->next = buffer->next; }
-		if (buffer->next == NULL) { nodes_tail = buffer->prev; }
-		else { buffer->next->prev = buffer->prev; }
-		free(buffer);
-		
-		//Pruning 2: Remove objects farther than furthest point of nearest MBR
-		Branch *ll_pointer2 = objects_head;
-		while (ll_pointer2 != NULL) {
-			if (ll_pointer2->MinDist > nodes_minminmax) {
-				//Remove link
-				Branch *buffer2 = ll_pointer2;
-				ll_pointer2 = ll_pointer2->next;
-				if (buffer2->prev == NULL) { objects_head = buffer2->next; }
-				else { buffer2->prev->next = buffer2->next; }
-				if (buffer2->next == NULL) { objects_tail = buffer2->prev; }
-				else { buffer2->next->prev = buffer2->prev; }
-				free(buffer2);
-			} else { ll_pointer2 = ll_pointer2->next; }
-		}
-
-		//Pruning 3: Remove MBRs farther than furthest point of nearest object
-		ll_pointer2 = nodes_head;
-		float objs_minminmax = pruning_min_minmaxdist(objects_head);
-		while (ll_pointer2 != NULL) {
-			if (ll_pointer2->MinDist > objs_minminmax) {
-				//Remove link
-				Branch *buffer2 = ll_pointer2;
-				ll_pointer2 = ll_pointer2->next;
-				if (buffer2->prev == NULL) { nodes_head = buffer2->next; }
-				else { buffer2->prev->next = buffer2->next; }
-				if (buffer2->next == NULL) { nodes_tail = buffer2->prev; }
-				else { buffer2->next->prev = buffer2->prev; }
-				free(buffer2);
-			} else { ll_pointer2 = ll_pointer2->next; }
-		}
-	}
-	//At this point: only objects are left as nn candidates.
-	ll_pointer = objects_head;
-	while (ll_pointer != NULL) {
-		if (ll_pointer->MinDist < nnDist) {
-			nnDist = ll_pointer->MinDist;
-			nn = ll_pointer;
-		}
-		ll_pointer = ll_pointer->next;
-	}
-	//free linked lists.
-	ll_pointer = nodes_head;
-	while (ll_pointer != NULL) {
-		Branch *buffer = ll_pointer;
-		ll_pointer = ll_pointer->next;
-		if (buffer != nn) { free(buffer); }
-	}
-	ll_pointer = objects_head;
-	while (ll_pointer != NULL) {
-		Branch *buffer = ll_pointer;
-		ll_pointer = ll_pointer->next;
-		if (buffer != nn) { free(buffer); }
-	}
-	return nn;
+	return NULL;
 }
 
 float pruning_min_minmaxdist(Branch *linkedlist) { //Pass in the head of a linked list
