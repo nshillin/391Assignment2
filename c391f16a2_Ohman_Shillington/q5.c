4f5a98442edb2837f0fafe9e0e46eb4f485e85a1
Noah Shillington <nshillin@ualberta.ca>
2016-10-31 10:48:18 -0600

    q5 fixed

--- c391f16a2_Ohman_Shillington/q5.c
+++ c391f16a2_Ohman_Shillington/q5.c
@@ -24,32 +24,32 @@ int main(int argc, char **argv) {
 		sqlite3_close(db);
 		return(1);
 	}
-	
+
 	check = create_indexes(db);
 	if (check) { return 1; }
-	
+
 	srand(time(NULL));
 	int l = atoi(argv[1]);
 	int boxes[100][4];
-
-	for (int i = 0; i < 100; i++) {
+	int i;
+	for (i = 0; i < 100; i++) {
 		boxes[i][0] = rand() % (999 - l); //Returns a number for the min coordinate between 0 and 999-l (leaving room for dimensions)
 		boxes[i][1] = boxes[i][0] + l;
 		boxes[i][2] = rand() % (999 - l); //As above
 		boxes[i][3] = boxes[i][2] + l;
 	}
-	
+
 	struct timespec start, finish;
 	char select_string[255];
 
 	clock_gettime(CLOCK_REALTIME, &start);
-	
-	for (int i = 0; i < 100; i++) {
+	for (i = 0; i < 100; i++) {
 		check = sprintf(select_string, "SELECT COUNT(*) FROM poirtree \
 				WHERE minX >= %i AND maxX <= %i \
 				AND minY >= %i AND maxY <= %i;",
 				boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]);
-		for (int j = 0; j < 20; j++) {
+		int j;
+		for (j = 0; j < 20; j++) {
 			check = sqlite3_prepare_v2(db, select_string, -1, &stmt, 0);
 			if(check != SQLITE_OK) {
 				fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
@@ -63,15 +63,15 @@ int main(int argc, char **argv) {
 	clock_gettime(CLOCK_REALTIME, &finish);
 	//1000 ms/s / 2000 queries = 1/2
 	float avgTime_rTree = ((float)(finish.tv_sec - start.tv_sec) + (float)(finish.tv_nsec - start.tv_nsec)/1000000000) / 2;
-	
+
 	clock_gettime(CLOCK_REALTIME, &start);
-	
-	for (int i = 0; i < 100; i++) {
+	for (i = 0; i < 100; i++) {
 		check = sprintf(select_string, "SELECT COUNT(*) FROM poi \
 				WHERE minX >= %i AND maxX <= %i \
 				AND minY >= %i AND maxY <= %i;",
 				boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]);
-		for (int j = 0; j < 20; j++) {
+		int j;
+		for (j = 0; j < 20; j++) {
 			check = sqlite3_prepare_v2(db, select_string, -1, &stmt, 0);
 			if(check != SQLITE_OK) {
 				fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
@@ -82,13 +82,13 @@ int main(int argc, char **argv) {
 			sqlite3_finalize(stmt);
 		}
 	}
-	
+
 	clock_gettime(CLOCK_REALTIME, &finish);
 	float avgTime_control = ((float)(finish.tv_sec - start.tv_sec) + (float)(finish.tv_nsec - start.tv_nsec)/1000000000) / 2;
-	
+
 	printf("Parameter l: %i\nAverage runtime with r-tree: %f ms\nAverage runtime without r-tree: %f ms\n",
 			l, avgTime_rTree, avgTime_control);
-	
+
 	return(0);
 }
 
@@ -104,7 +104,7 @@ int create_indexes(sqlite3 *db) {
 			ON poi (minY);";
 	char *query4 = "CREATE INDEX IF NOT EXISTS maxY_index \
 			ON poi (maxY);";
-	
+
 	check = sqlite3_prepare_v2(db, query1, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
@@ -117,7 +117,7 @@ int create_indexes(sqlite3 *db) {
 		return(1);
 	}
 	sqlite3_finalize(stmt);
-	
+
 	check = sqlite3_prepare_v2(db, query2, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
@@ -130,7 +130,7 @@ int create_indexes(sqlite3 *db) {
 		return(1);
 	}
 	sqlite3_finalize(stmt);
-	
+
 	check = sqlite3_prepare_v2(db, query3, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
@@ -143,7 +143,7 @@ int create_indexes(sqlite3 *db) {
 		return(1);
 	}
 	sqlite3_finalize(stmt);
-	
+
 	check = sqlite3_prepare_v2(db, query4, -1, &stmt, 0);
 	if(check != SQLITE_OK) {
 		fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
